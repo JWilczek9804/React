7b0f48bf515cefcef59767e5a690eed9379b0a17
JWilczek9804 <167343233+JWilczek9804@users.noreply.github.com>
2025-01-09 23:58:14 +0100

    In proccess

--- 11-WorldWise/src/App.jsx
+++ 11-WorldWise/src/App.jsx
@@ -11,11 +11,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/pricing" element={<Pricing />} />
+        {/* "Index" parameter replaces path="/", so it's a default option of nested routes */}
+        <Route index element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>LIST</p>} />{" "}
+          <Route path="cities" element={<p>Cities</p>} />
+          <Route path="countries" element={<p>Countires</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
